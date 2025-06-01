21ebb16100e15c9aff1a7835d7abc0f41803c055
Niroshan-k <lakshanniroshan822@gmail.com>
2025-06-01 11:23:59 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://dimoraland.onrender.com/build/',
+    base: '/build/',
     plugins: [
         laravel({
             input: [
